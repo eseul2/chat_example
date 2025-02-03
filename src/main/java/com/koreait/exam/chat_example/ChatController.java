82de9d80b4ccfb7fe28020a49814e12dc9a587bb
eseul2 <dltmfsus@naver.com>
2025-02-03 15:32:44 +0900

    제네릭과 설정

--- src/main/java/com/koreait/exam/chat_example/ChatController.java
+++ src/main/java/com/koreait/exam/chat_example/ChatController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/chat")
 public class ChatController {
 
-    @GetMapping("/writeMessage")
+    // RsData에서 정하지 않은 타입이 여기서 정해졌다.
+    ChatMessage message = new ChatMessage("김철수","안녕");
+
+    @PostMapping("/writeMessage")
     @ResponseBody
-    public String writeMessage() {
-        return "메세지 작성됨";
+    //그래서 <> 이렇게 안에 지정을 했음 제네릭 찾아봐라
+    public RsData<ChatMessage> writeMessage() {
+        return new RsData<>("S-1","메세지가 작성됨", message);
     }
 }
+
+
 
 
 
---
+++ src/main/java/com/koreait/exam/chat_example/RsData.java
@@ -0,0 +1,18 @@
+package com.koreait.exam.chat_example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RsData<T> {
+    private String resultCode;
+    private String msg;
+    private T data; // 타입을 나중에 정하기 위해서 제네릭
+}
+
+
+
+

10fe56b8b42bff29ee940f3fccfdb61f5f69dec2
eseul2 <dltmfsus@naver.com>
2025-02-04 10:31:05 +0900

    채팅 어디까지 읽었냐, stream, filter

--- src/main/java/com/koreait/exam/chat_example/ChatController.java
+++ src/main/java/com/koreait/exam/chat_example/ChatController.java
@@ -2,13 +2,16 @@ package com.koreait.exam.chat_example;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
 
     //채팅 메시지를 저장하는 리스트 (메모리 내 저장소 역할 아직 db를 안만들어서..)
@@ -62,6 +65,11 @@ public class ChatController {
         );
     }
 
+
+    public record messagesRequest(Long fromId) {
+
+    }
+
     // 채팅 메세지 리스트를 반환하는 응답 데이터 클래스
     public record messagesResponse(List<ChatMessage> messages, long count) {
 
@@ -71,7 +79,26 @@ public class ChatController {
     // 현재까지 저장된 메세지를 리스트 형태로 반환
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<messagesResponse> messages() {
+    public RsData<messagesResponse> messages(messagesRequest req) {
+
+        // 상단에 전역변수를 이용해서 지역변수로 사용함
+        List<ChatMessage> messages = chatMessages;
+
+        log.debug("req : {}", req );  // 디버깅 할 수 있음. 상단에 어노테이션 붙여야됨 @Slf4j
+
+        // 번호가 같이 입력되었다면?
+        // 걍 리스트 내놔! 이게 아니라 조건에 맞춰서 내놔라~
+        if(req.fromId != null) {
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId) // 말그대로 필터임. 
+                    .findFirst().orElse(-1); // 아예 없는 인덱스 번호인 -1
+
+            if(index != -1) {
+                messages = messages.subList(index + 1, messages.size());
+            }
+
+        }
+
         return new RsData<>(
                 "S-1",
                 "메세지 리스트",
